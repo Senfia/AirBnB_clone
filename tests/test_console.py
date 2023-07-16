25a159688a839b97dd358d4a0638f19d47019d37
Senyo Fiawornu <contactsenyo@gmail.com>
2023-07-16 18:26:07 +0000

    new functions

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,86 +1,54 @@
 #!/usr/bin/python3
 """
 Tests console's functionality
-
 """
 import sys
 import unittest
-from unittest import mock
-from unittest.mock import create_autospec
-from unittest.mock import patch
-from io import StringIO
+import inspect
+import io
+import pycodestyle
+from contextlib import redirect_stdout
 from console import HBNBCommand
 
 
-class TestConsole(unittest.TestCase):
+class TestHBNBCommand(unittest.TestCase):
     """
-    Automated tests for interactive shell based on cmd module
+    class for testing HBNBCommand
     """
-    def setUp(self):
-        """setup method for Console Test Class"""
-        self.mock_stdin = create_autospec(sys.stdin)
-        self.mock_stdout = create_autospec(sys.stdout)
 
-    def create(self, server=None):
-        """Create command"""
-        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
+    @classmethod
+    def setUpClass(cls):
+        """
+        Class method for the doc tests
+        """
+        cls.setup = inspect.getmembers(HBNBCommand, inspect.isfunction)
 
-    def test_exit(self):
-        """exit command"""
-        cli = self.create()
-        self.assertTrue(cli.onecmd("quit"))
+    def test_pycodestyle_HBNBCommand(self):
+        """
+        checks conformity with pycodestyle
+        """
+        pep8style = pycodestyle.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/console.py'])
+        self.assertEqual(result.total_errors, 1,
+                         "Found code style errors (and warnings).")
 
-    def test_EOF(self):
-        """exit command"""
-        cli = self.create()
-        self.assertTrue(cli.onecmd("EOF"))
+    def test_pycodestyle_test_HBNBCommand(self):
+        """
+        checks conformity with pycodestyle
+        """
+        pep8style = pycodestyle.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_console.py'])
+        self.assertEqual(result.total_errors, 1,
+                         "Found code style errors (and warnings).")
 
-    def test_help(self):
-        """test help command"""
+    def test_module_docstring(self):
+        """
+        Tests module docstring documentation
+        """
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1)
 
-    def test_exit(self):
-        """exit command"""
-        cli = self.create()
-        self.assertTrue(cli.onecmd("quit"))
-
-    def test_EOF(self):
-        """exit command"""
-        cli = self.create()
-        self.assertTrue(cli.onecmd("EOF"))
-
-    def test_create_object(self):
-        """test do_create"""
-        cli = self.create()
-        with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            self.assertFalse(cli.onecmd('create'))
-        self.assertEqual('** class name missing **',
-                         fakeOutput.getvalue().strip())
-
-
-    def test_destroy_object(self):
-        """destroyerrr"""
-        cli = self.create()
-        bad_input = 'destroy BaseModel JuiceyName-RandomName-98-9899087'
-        with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            self.assertFalse(cli.onecmd(bad_input))
-        self.assertEqual('** no instance found **', '** no instance found **')
-
-    def test_update(self):
-        """test do_update"""
-        cli = self.create()
-        bad_input = 'update BaseModel JuiceyName-Randomname-98-989899087 age'
-        with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            self.assertFalse(cli.onecmd(bad_input))
-        self.assertEqual('** value missing **', '** value missing **')
-
-    def test_all(self):
-        """test do_all"""
-        cli = self.create()
-        with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            self.assertFalse(cli.onecmd('classname.all()'))
-        self.assertEqual("** class doesn't exist **",
-                         "** class doesn't exist **")
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_class_docstring(self):
+        """
+        Tests class docstring documentation
+        """
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1)
